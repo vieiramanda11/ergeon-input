5f5904c72fd93e39611744300d70a31a8d9b8f6e
vieiramanda11 <amandavieirazevedo@gmail.com>
2022-07-26 15:14:03 -0300

    Finish input

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from 'react';
+import InvalidIcon from './components/Icons/Invalid.icon';
+import ValidIcon from './components/Icons/Valid.icon';
+import './styles.css';
 
 function App() {
+  const [value, setValue] = useState('');
+  var isValidZip = useMemo(
+    () => value.match(/(^\d{5}$)|(^\d{5}-\d{4}$)/),
+    [value]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='zipcode'>
+        <input
+          type='number'
+          id='zipcode'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <label htmlFor='zipcode'>Type in a valid american zip code...</label>
+        <span className='indicator'>
+          {isValidZip ? <ValidIcon /> : <InvalidIcon />}
+        </span>
+      </div>
     </div>
   );
 }
